7f21e3e71a0b0142c5190425f9cd0c055c751007
Dominic Barnes <dominic@dbarnes.info>
2020-08-20 23:51:50 -0600

    feat: add subtests helper

--- filesystem.go
+++ filesystem.go
@@ -2,8 +2,23 @@ package got
 
 import (
 	"io/ioutil"
+	"path/filepath"
 	"testing"
 )
+
+// RunSubTests is a helper for executing a sub-test for each directory within a
+// base directory. This is useful in cases where each sub-folder is a separate
+// test case.
+func RunSubTests(t *testing.T, dir string, sub func(*testing.T, string, string)) {
+	t.Helper()
+	for _, testName := range ListSubDirs(t, dir) {
+		testName := testName
+		testDir := filepath.Join(dir, testName)
+		t.Run(testName, func(t *testing.T) {
+			sub(t, testName, testDir)
+		})
+	}
+}
 
 // ListSubDirs finds the only the nested directories within the input dir. This
 // is useful for quickly grabbing a list of subtests represented by
